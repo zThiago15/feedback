f53d104cc06e4abc27eced93443ad2e368ca7629
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-05-07 15:24:48 -0300

    criando funcao para tirar screenshot da tela usando html2canvas

---
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -0,0 +1,22 @@
+import { Camera } from "phosphor-react";
+import html2canvas from 'html2canvas';
+
+export function ScreenshotButton() {
+
+  async function handleTakeScreenshot() {
+    const  canvas = await html2canvas(document.querySelector('html')!);
+    const base64image = canvas.toDataURL('image/png'); // texto representando imagem
+    console.log(base64image);
+    
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={ handleTakeScreenshot }
+      className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+    >
+      <Camera className="w-6 h-6 text-zinc-100" />
+    </button>
+  )
+}
--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,15 +1,26 @@
+import { ArrowLeft, Camera } from 'phosphor-react';
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from '../ScreenshotButton';
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) {
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
   const feedbackTypeInfo  = feedbackTypes[feedbackType];
 
   return(
     <>
+    <button 
+      type="button" 
+      className="top-5 left-5 absolute text-zinc-500 hover:text-zinc-100"
+      onClick={ onFeedbackRestartRequested }
+    >
+      <ArrowLeft weight="bold" className="w-4 h-4" />
+    </button>
+
     <header>
       <span className="text-xl leading-6 flex items-center gap-2">
         <img src={ feedbackTypeInfo.image.source } alt={ feedbackTypeInfo.image.alt } className="w-6 h-6" />
@@ -19,9 +30,24 @@ export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) 
       <CloseButton />
     </header> 
 
-    <div className="flex py-8 gap-2 w-full">
+    <form className="my-4 w-full">
+      <textarea
+        className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+        placeholder="Conte com detalhes o que está acontecendo..."
+      />
 
-    </div>
+      <footer className="flex gap-2 mt-2">
+
+        <ScreenshotButton />
+
+        <button
+          type="submit"
+          className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+        >
+          Enviar feedback
+        </button>
+      </footer>
+    </form>
   </>
   );
 }
