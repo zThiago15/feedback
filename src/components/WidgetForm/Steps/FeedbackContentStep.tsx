b92cb511d1d7899394799ce2a9e3e6d7e30ff3ab
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-05-09 03:00:09 -0300

    adicionando componente de loading e axios para integracao com back-end

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeft, Camera } from 'phosphor-react';
 import { FormEvent, useState } from 'react';
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from '../../../libs/api';
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from '../ScreenshotButton';
 
@@ -22,12 +23,17 @@ export function FeedbackContentStep({
 
   const feedbackTypeInfo  = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({
+ /*    console.log({
       screenshot,
       comment,
-    });  
+    });  */ 
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot
+    })
 
     onFeedbackSent();
   }
